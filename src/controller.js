14469b8b7de9b0a980c482c60f5189feaf5dad36
Alex Pinchi <alex.pinchi@gmail.com>
2024-07-05 10:31:38 -0400

    exportar datos

--- src/controller.js
+++ src/controller.js
@@ -20,6 +20,28 @@ export const mostrarEmpleados = async (request, response) => {
     response.end(JSON.stringify(rows))
   } catch (error) {
     response.writeHead(500, { 'Content-Type': 'application/json; charset=utf-8' })
-    response.end(JSON.stringify({ error: 'No se puidieron mostrar los empleados' }))
+    response.end(JSON.stringify({ error: 'No se pudieron mostrar los empleados' }))
+  }
+}
+
+export const exportarEmpleados = async (request, response) => {
+  try {
+    const [rows] = await pool.execute('SELECT * FROM usuarios')
+
+    const data = rows.map(row => JSON.stringify(row)).join('\n')
+    fs.writeFile('empleados.txt', data, err => {
+      if (err) {
+        response.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' })
+        response.end('Error al exportar los empleados')
+        console.error('Error al escribir en el archivo empleados.txt', err)
+      } else {
+        response.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' })
+        response.end('Datos de empleados exportados correctamente')
+        console.log('Datos de empleados guardados en empleados.txt')
+      }
+    })
+  } catch (error) {
+    response.writeHead(500, { 'Content-Type': 'application/json; charset=utf-8' })
+    response.end(JSON.stringify({ error: 'No se pudieron exportar los empleados' }))
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import http from 'http'
 import { PORT } from './config.js'
-import { bienvenida, mostrarEmpleados } from './controller.js'
+import { bienvenida, mostrarEmpleados, exportarEmpleados } from './controller.js'
 
 const requestHandler = (request, response) => {
   console.log(`Solicitud ${request.method} ${request.url}`)
@@ -9,14 +9,16 @@ const requestHandler = (request, response) => {
     bienvenida(request, response)
   } else if (request.method === 'GET' && request.url === '/empleados') {
     mostrarEmpleados(request, response)
+  } else if (request.method === 'POST' && request.url === '/export') {
+    exportarEmpleados(request, response)
   } else {
-    response.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' })
-    response.end('No se encontro la ruta')
+    response.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' })
+    response.end('No se encontró la ruta')
   }
 }
 
 const server = http.createServer(requestHandler)
 
 server.listen(PORT, () => {
-  console.log(`Servidor escuchado en http://localhost:${PORT}`)
+  console.log(`Servidor escuchando en http://localhost:${PORT}`)
 })
